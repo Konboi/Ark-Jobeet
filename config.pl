6aea2476cb449fd44e6fef56f8990f1bfc29c2a2
Konboi <ryosuke.yabuki@gmail.com>
2016-03-30 12:46:08 +0900

    Add cache config

--- config.pl
+++ config.pl
@@ -12,4 +12,9 @@ return {
     active_days          => 30,
     max_jobs_on_homepage => 10,
     max_jobs_on_category => 20,
+
+    cache => {
+        share_file     => $home->file('tmp', 'cache')->stringify,
+        unlink_on_exit => 0,
+    },
 };
