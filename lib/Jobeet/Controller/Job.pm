412732923522e2ae148194af514c16af383e49b9
Konboi <ryosuke.yabuki@gmail.com>
2016-03-30 15:36:46 +0900

    Add atom feed

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -3,6 +3,7 @@ use Ark 'Controller';
 with 'Ark::ActionClass::Form';
 
 use Jobeet::Models;
+use DateTime::Format::W3CDTF;
 
 sub index :Path {
     my ($self, $c) = @_;
@@ -82,7 +83,13 @@ sub publish :Chained('job') :PathPart {
 
 sub atom :Local {
     my ($self, $c) = @_;
+
     $c->res->content_type('application/atom+xml; charset=utf-8');
+
+    $c->stash->{w3c_date}    = DateTime::Format::W3CDTF->new;
+    $c->stash->{latest_post} = models('Schema::Job')->latest_post;
+
+    $c->forward('index');
 }
 
 1;
